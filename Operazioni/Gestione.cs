3f7c780bb927accef8215ddce8bde67af58d032f
miglio000 <gabrielemiglio23@gmail.com>
2021-02-22 10:33:14 +0100

    Add project files.

---
+++ Operazioni/Gestione.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Operazioni
+{
+    public class Gestione
+    {
+        public static void BubbleSort(double[] n, int tanti)
+        {
+            int x, y;
+            double temp;
+            for (x = 0; x < tanti - 1; x++)
+            {
+                for (y = 0; y < tanti - 1 - x; y++)
+                {
+                    if (n[y] > n[y + 1])
+                    {
+                        temp = n[y];
+                        n[y] = n[y + 1];
+                        n[y + 1] = temp;
+                    }
+                }
+            }
+        }
+        public static void InserisciRandom(double[] array)
+        {
+            Random r = new Random();
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = r.NextDouble() * 1000;
+            }
+        }
+        public static void StampaArray(double[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+        public static double MaxTraElementi(double[] array)
+        {
+            double max = double.MinValue;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                    max = array[i];
+            }
+            return max;
+        }
+        public static double MinTraElementi(double[] array)
+        {
+            double min = double.MaxValue;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                    min = array[i];
+            }
+            return min;
+        }
+    }
+}
---
+++ OrdinamentoStipendi/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Operazioni;
+namespace OrdinamentoStipendi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci dimensione array: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            double[] array = new double[x];
+            Gestione.InserisciRandom(array);
+            Gestione.BubbleSort(array, x);
+            Console.WriteLine($"Gli stipendi ordinati sono: ");
+            Gestione.StampaArray(array);
+            double max = Gestione.MaxTraElementi(array);
+            double min = Gestione.MinTraElementi(array);
+            Console.WriteLine($"Il max degli stipendi è: {max}, il min è {min}.");
+        }
+    }
+}
